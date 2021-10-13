15d55825e39a67b42cca44e2c2076613b9a0ff74
niubrandon <niubrandon@gmail.com>
2021-10-13 16:37:35 -0400

    added seeds

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,49 @@
+INSERT INTO users (id, name, email, password) VALUES(1, 'Eva', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (id, name, email, password) VALUES(2, 'Louisa Meyer', 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (id, name, email, password) VALUES(3, 'Dominic Parks', 'victoriablackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (id, name, email, password) VALUES(4, 'Sue Luna', 'jasonvincent@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (id, name, email, password) VALUES(5, 'Rosalie Garza', 'jacksondavid@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (id, name, email, password) VALUES(6, 'Etta West', 'charlielevy@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (id, name, email, password) VALUES(7, 'Margaret Wong', 'makaylaweiss@icloud.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (id, name, email, password) VALUES(8, 'Leroy Hart', 'jaycereynolds@inbox.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+
+
+INSERT INTO properties (id, owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active) VALUES(1, 1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 930.61, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', '28142', true ),
+(2, 1, 'Blank corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234, 6, 6, 7, 'Canada', '615 Nami Road', 'Bohbatev', 'Alberta', '83680', true ),
+(3, 2, 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', '44583', true ),
+(4, 4, 'Headed know', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', '44583', true ),
+(5, 6, 'Port out', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', '44583', true ),
+(6, 6, 'Fun glad', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', '44583', true ),
+(7, 7, 'Shine twenty', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', '44583', true ),
+(8, 8, 'Game fill', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', '44583', true );
+
+
+
+INSERT INTO reservations (id, start_date, end_date, property_id, guest_id) VALUES(1, '2018-09-11', '2019-09-26', 2, 3),
+(2, '2019-01-04', '2019-02-01', 2, 2),
+(3, '2021-10-01', '2021-10-14', 1, 4),
+(4, '2014-10-21', '2014-10-21', 3, 5),
+(5, '2016-07-17', '2016-08-01', 3, 4),
+(6, '2018-05-01', '2018-05-27', 4, 8),
+(7, '2022-10-04', '2022-10-23', 5, 1),
+(8, '2015-09-13', '2015-09-30', 6, 8),
+(9, '2023-05-27', '2023-05-28', 4, 2),
+(10, '2023-04-23', '2023-05-02', 8, 1);
+
+
+INSERT INTO property_reviews (id, guest_id, property_id, reservation_id, rating, message) VALUES(1, 2, 5, 10, 3, "love this places"),
+(2, 1, 4, 1, 4, "love this places"),
+(3, 8, 1, 2, 4, "love this places"),
+(4, 3, 8, 5, 4, "love this places"),
+(5, 4, 2, 7, 5, "love this places"),
+(6, 4, 3, 4, 4, "love this places"),
+(7, 5, 6, 3, 5, "love this places");
+
+
+
